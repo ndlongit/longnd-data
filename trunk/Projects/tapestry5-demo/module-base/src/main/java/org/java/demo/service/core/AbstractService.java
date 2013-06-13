820a354de9db5d4b0cc6dcf32171285d364e6a1d
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2013-06-13 16:56:58 +0000

    Use dynamic Logging Level for debug

--- trunk/Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/service/core/AbstractService.java
+++ trunk/Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/service/core/AbstractService.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.java.demo.constant.AppConstants;
 import org.java.demo.dao.core.BasicDao;
 import org.java.demo.exception.DataConstraintException;
 import org.java.demo.model.core.BasicEntity;
@@ -25,7 +26,7 @@ public abstract class AbstractService<T extends BasicEntity<?>, ID extends Seria
         return dao;
     }
 
-    // Do not use @Autowired on field directly, otherwise error appear
+    // Do not use @Autowired on field directly, otherwise error will appear
     @Autowired
     public void setDao(DAO dao) {
         this.dao = dao;
@@ -33,85 +34,85 @@ public abstract class AbstractService<T extends BasicEntity<?>, ID extends Seria
 
     @Override
     public T find(ID id) {
-        logger.debug("find()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "find()" + METHOD_BEGIN);
         T result = (T) dao.find(id);
-        logger.debug("find()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "find()" + METHOD_END);
         return result;
     }
 
     @Override
     public List<T> findByIds(List<T> ids) {
-        logger.debug("findByIds()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "findByIds()" + METHOD_BEGIN);
         List results = dao.findByIds(ids);
-        logger.debug("findByIds()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "findByIds()" + METHOD_END);
         return results;
     }
 
     @Override
     public List<T> findAll() {
-        logger.debug("findAll()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "findAll()" + METHOD_BEGIN);
         List<T> results = dao.findAll();
-        logger.debug("findAll()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "findAll()" + METHOD_END);
         return results;
     }
 
     @Override
     public List<ID> findAllIds() {
-        logger.debug("findAllIds()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "findAllIds()" + METHOD_BEGIN);
         List results = dao.findAllIds();
-        logger.debug("findAllIds()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "findAllIds()" + METHOD_END);
         return results;
     }
 
     @Override
     public void delete(ID id) {
-        logger.debug("delete(ID id)" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "delete(ID id)" + METHOD_BEGIN);
         T entity = (T) dao.find(id);
         delete(entity);
-        logger.debug("delete(ID id)" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "delete(ID id)" + METHOD_END);
     }
 
     @Override
     public void delete(T entity) {
-        logger.debug("delete(T entity)" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "delete(T entity)" + METHOD_BEGIN);
         dao.delete(entity);
-        logger.debug("delete(T entity)" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "delete(T entity)" + METHOD_END);
     }
 
     @Override
     public T save(T entity) throws DataConstraintException, Exception {
-        logger.debug("save()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "save()" + METHOD_BEGIN);
         try {
             dao.save(entity);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             throw e;
         }
-        logger.debug("save()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "save()" + METHOD_END);
         return entity;
     }
 
     @Override
     public T update(T entity) throws DataConstraintException, Exception {
-        logger.debug("update()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "update()" + METHOD_BEGIN);
         T result = (T) dao.update(entity);
-        logger.debug("update()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "update()" + METHOD_END);
         return result;
     }
 
     @Override
     public List<T> findByProperty(String propertyName, List propertyValues) {
-        logger.debug("findByProperty()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "findByProperty()" + METHOD_BEGIN);
         List results = dao.findByProperty(propertyName, propertyValues);
-        logger.debug("findByProperty()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "findByProperty()" + METHOD_END);
         return results;
     }
 
     @Override
     public T findUniqueByProperty(String propertyName, Object propertyValue) {
-        logger.debug("findUniqueByProperty()" + METHOD_BEGIN);
+        logger.log(AppConstants.getTrackLevel(), "findUniqueByProperty()" + METHOD_BEGIN);
         T result = (T) dao.findUniqueByProperty(propertyName, propertyValue);
-        logger.debug("findUniqueByProperty()" + METHOD_END);
+        logger.log(AppConstants.getTrackLevel(), "findUniqueByProperty()" + METHOD_END);
         return result;
     }
 }
