5ae4395eb905076335d22ade0d5230ca93917f2c
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2013-10-09 04:02:31 +0000

    Modify Account to use Role object instead

--- Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/model/Role.java
+++ Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/model/Role.java
@@ -9,4 +9,17 @@ import org.java.demo.model.core.SimpleEntity;
 @Entity
 @AttributeOverride(name = SimpleEntity.PROP_NAME, column = @Column(name = "role_name", unique = true, nullable = false))
 public class Role extends SimpleEntity {
+
+    public static final String PROP_VALUE = "value";
+    
+    private String value;
+
+    @Column(name = "role_value", nullable = false, unique = true)
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
