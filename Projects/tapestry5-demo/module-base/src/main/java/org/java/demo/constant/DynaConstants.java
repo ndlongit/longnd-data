48bc306fae93506dce7fcb95eaddb6f169215b5e
NDLONGIT <NDLONGIT@0678d5b6-dab6-a150-5438-a0108a10141d>
2013-10-09 03:13:15 +0000

    Change Log level

--- Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/constant/DynaConstants.java
+++ Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/constant/DynaConstants.java
@@ -6,11 +6,11 @@ public abstract class DynaConstants {
 
     // This logging will be use to output information to debug application
     public static Level getTrackLevel() {
-        return new LogLevel(Level.FATAL_INT + 1);
+        return new LogLevel(Level.DEBUG_INT + 1);
     }
 }
 
-class LogLevel extends Level {
+final class LogLevel extends Level {
 
     public LogLevel(int level) {
         super(level, "TRACK", 0);
