bb41b7540e2a16025b783400c3630c791f91a5ec
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-01-18 17:17:25 +0000

    Tiles is OK

---
+++ Projects/tapestry5-demo/module-struts2/src/main/java/org/java/demo/action/PersonAction.java
@@ -0,0 +1,78 @@
+package org.java.demo.action;
+
+import java.util.List;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.java.demo.model.Person;
+import org.java.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.Preparable;
+
+@Results({ @Result(name = "list", location = "list.jsp"), @Result(name = "edit", location = "/index.jsp") })
+public class PersonAction implements Preparable {
+
+    @Autowired
+    private PersonService service;
+    private List<Person> persons;
+    private Person person;
+    private Integer id;
+
+    public String execute() {
+        this.persons = service.findAll();
+        return "list";
+    }
+
+    @Action("list")
+    public String list() {
+        return execute();
+    }
+
+    @Action("save")
+    public String save() {
+        this.service.save(person);
+        return execute();
+    }
+
+    @Action("edit")
+    public String edit() {        
+        if (id != null) {
+            person = service.find(id);
+        }
+        return "edit";
+    }
+
+    @Action("remove")
+    public String remove() {
+        service.remove(id);
+        return execute();
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void prepare() throws Exception {
+        if (id != null) {
+            person = service.find(id);
+        }
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
