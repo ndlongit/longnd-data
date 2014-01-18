7b97f7ccd218866ca39fe4a53dc806e4a9825f4d
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-01-18 17:46:38 +0000

    Make Services extending Core Module

--- trunk/Projects/tapestry5-demo/module-struts2/src/main/java/org/java/demo/dao/PersonDaoImpl.java
+++ trunk/Projects/tapestry5-demo/module-struts2/src/main/java/org/java/demo/dao/PersonDaoImpl.java
@@ -4,6 +4,7 @@ import org.java.demo.dao.core.AbstractDao;
 import org.java.demo.model.Person;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public class PersonDaoImpl extends AbstractDao<Person, Long> implements PersonDao {
 }
