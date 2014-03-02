d57d2e709f420024cbf83f73c1a71fe047d5c571
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-03-02 06:58:49 +0000

    Extends some Spring Security Filters

---
+++ trunk/Projects/tapestry5-demo/module-spring-security/src/main/java/org/java/demo/filter/spring/AuthenticationFilterEntryPoint.java
@@ -0,0 +1,6 @@
+package org.java.demo.filter.spring;
+
+import org.springframework.security.ui.webapp.AuthenticationProcessingFilterEntryPoint;
+
+public class AuthenticationFilterEntryPoint extends AuthenticationProcessingFilterEntryPoint {
+}
