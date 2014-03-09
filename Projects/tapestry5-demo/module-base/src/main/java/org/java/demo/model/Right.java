90b13012ed1caa11c96892ae0cbb576b66b08b68
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-03-09 14:08:33 +0000

    Append "s" character to create table successfully

--- Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/model/Right.java
+++ Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/model/Right.java
@@ -1,9 +1,11 @@
 package org.java.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import org.java.demo.model.core.SimpleEntity;
 
 @Entity
+@Table(name = "Rights")
 public class Right extends SimpleEntity {
 }
