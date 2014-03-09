1f61d8f86d0525215eaa0fb379c13e2c7f397995
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-03-09 14:08:33 +0000

    Append "s" character to create table successfully

--- trunk/Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/model/Group.java
+++ trunk/Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/model/Group.java
@@ -3,10 +3,12 @@ package org.java.demo.model;
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import org.java.demo.model.core.SimpleEntity;
 
 @Entity
+@Table(name = "Groups")
 @AttributeOverride(name = SimpleEntity.PROP_NAME, column = @Column(name = "group_name"))
 public class Group extends SimpleEntity {
 }
