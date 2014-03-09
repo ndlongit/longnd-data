d750c0d75e8e4924903106c2e5b3d8bced7aa568
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-03-09 09:24:40 +0000

    Re-structure some packages

---
+++ trunk/Projects/tapestry5-demo/module-spring-security/src/main/java/org/java/demo/util/handler/spring/AccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package org.java.demo.util.handler.spring;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+
+public class AccessDeniedHandler extends org.springframework.security.web.access.AccessDeniedHandlerImpl {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException,
+            ServletException {
+
+        // TODO re-implement this method to forward to Struts2 actions
+         super.handle(request, response, accessDeniedException);
+    }
+
+    @Override
+    public void setErrorPage(String errorPage) {
+        if (errorPage == null || errorPage.trim().length() == 0) {
+            errorPage = "/access-denied.jsp";
+        }
+        super.setErrorPage(errorPage);
+    }
+
+}
