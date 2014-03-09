26e5a392b3bd189212d118b83f49170816a0595c
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-03-09 13:38:01 +0000

    Naming Strategy

--- Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/util/hibernate/NamingStrategy.java
+++ Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/util/hibernate/NamingStrategy.java
@@ -4,10 +4,10 @@ import org.hibernate.cfg.ImprovedNamingStrategy;
 
 public class NamingStrategy extends ImprovedNamingStrategy {
 
-    public static final String TABLE_PREFIX = "tbl_";
-
-    @Override
-    public String classToTableName(String className) {
-        return TABLE_PREFIX + super.classToTableName(className);
-    }
+//    public static final String TABLE_PREFIX = "tbl_";
+//
+//    @Override
+//    public String classToTableName(String className) {
+//        return TABLE_PREFIX + super.classToTableName(className);
+//    }
 }
