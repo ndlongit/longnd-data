ce475f68e878c1af8f821483ba254b08203da1d1
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-04-05 11:12:43 +0000

    Lazy load work-around

--- trunk/Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/service/UserServiceImpl.java
+++ trunk/Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.java.demo.service;
 import java.util.List;
 
 import org.java.demo.dao.UserDao;
+import org.java.demo.model.Group;
 import org.java.demo.model.User;
 import org.java.demo.search.UserSearch;
 import org.java.demo.service.core.AbstractService;
@@ -13,8 +14,22 @@ public class UserServiceImpl extends AbstractService<User, Long, UserDao> implem
 
     @Override
     public List<User> search(UserSearch searchModel) {
-        
-        //FIXME Implement here
+
+        // FIXME Implement here
         return null;
+    }
+
+    @Override
+    public User find(Long id) {
+
+        // FIXME add OpenSessionInViewMode capacity
+        User user = dao.find(id);
+        List<Group> groups = user.getGroups();
+        for (Group group : groups) {
+
+        }
+        user.setGroups(groups);
+        return user;
+        // return super.find(id);
     }
 }
