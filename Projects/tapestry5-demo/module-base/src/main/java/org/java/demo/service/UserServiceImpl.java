c986cb2d3208c3895c0d228ffbbc937dae87dabb
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2014-04-05 17:32:05 +0000

    OpenSessionInViewMode is OK

--- Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/service/UserServiceImpl.java
+++ Projects/tapestry5-demo/module-base/src/main/java/org/java/demo/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package org.java.demo.service;
 import java.util.List;
 
 import org.java.demo.dao.UserDao;
-import org.java.demo.model.Group;
 import org.java.demo.model.User;
 import org.java.demo.search.UserSearch;
 import org.java.demo.service.core.AbstractService;
@@ -17,18 +16,5 @@ public class UserServiceImpl extends AbstractService<User, Long, UserDao> implem
 
         // FIXME Implement here
         return null;
-    }
-
-    @Override
-    public User find(Long id) {
-        User user = super.find(id);
-
-        // Load Lazy loading fields
-        List<Group> groups = user.getGroups();
-        if (groups != null) {
-            user.getGroups().size();
-        }
-
-        return user;
     }
 }
