808f27edd91b176d1f7a631bf18d3213ad95e9ec
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2015-01-16 07:13:51 +0000

    delete

--- trunk/Document/Liferay/mm/Extend Liferay Tables.mm
+++
@@ -1,4 +0,0 @@
-<map version="0.8.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1293102288751" ID="Freemind_Link_1934579638" MODIFIED="1293104904384" TEXT="Extend Liferay Tables"/>
-</map>
