9ba9401b4fd86948266c96c56d00a7db9414ad0a
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2015-01-16 07:13:51 +0000

    delete

--- Document/Liferay/mm/Generating Finder Methods.mm
+++
@@ -1,9 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1292861921052" ID="ID_803192852" MODIFIED="1292861930874" TEXT="Finder methods">
-<node CREATED="1292861934194" ID="ID_420630037" MODIFIED="1292862031666" POSITION="right" TEXT="findBy&lt;FieldName&gt;(fieldType &lt;fieldName&gt;)"/>
-<node CREATED="1292862090167" ID="ID_1984994812" MODIFIED="1292862134912" POSITION="right" TEXT="findBy&lt;FieldName&gt;(&lt;fieldType&gt; &lt;fieldName&gt;, int start, int end)"/>
-<node CREATED="1292862190041" ID="ID_1349845600" MODIFIED="1292862213163" POSITION="right" TEXT="removeBy&lt;FieldName&gt;(fieldType &lt;fieldName&gt;) "/>
-<node CREATED="1292862251194" ID="ID_1545884432" MODIFIED="1292862259406" POSITION="right" TEXT="countBy&lt;FieldName&gt;(fieldType &lt;fieldName&gt;) "/>
-</node>
-</map>
--- Document/Liferay/mm/creat a Portlet plugin using Ant.mm
+++
@@ -1,9 +0,0 @@
-<map version="0.8.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1292222227446" ID="Freemind_Link_734476408" MODIFIED="1292222273931" TEXT="creat a Portlet plugin using Ant">
-<node CREATED="1292222323666" ID="_" MODIFIED="1292222346119" POSITION="right" TEXT="Go to folder &quot;portlet&quot; of folder Plugin SDK"/>
-<node CREATED="1292222351494" ID="Freemind_Link_47499662" MODIFIED="1292222426369" POSITION="right" TEXT="Run bat command, ex: create.bat my-greeting &quot;My Greeting&quot;"/>
-<node CREATED="1292222463495" ID="Freemind_Link_1364135326" MODIFIED="1292222472120" POSITION="left" TEXT="deploy: ant deploy"/>
-<node CREATED="1292222472979" ID="Freemind_Link_639267667" MODIFIED="1292222494839" POSITION="left" TEXT="use porlet: login into Liferay Portal and go to Add / More / Sample.."/>
-</node>
-</map>
