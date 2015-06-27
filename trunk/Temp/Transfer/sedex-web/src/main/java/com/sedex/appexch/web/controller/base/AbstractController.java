783057ff0241dff0dfcb6a101a71c87b2af4bce3
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2015-06-27 04:49:22 +0000

    OK

--- trunk/Temp/Transfer/sedex-web/src/main/java/com/sedex/appexch/web/controller/base/AbstractController.java
+++ trunk/Temp/Transfer/sedex-web/src/main/java/com/sedex/appexch/web/controller/base/AbstractController.java
@@ -1,7 +1,6 @@
 package com.sedex.appexch.web.controller.base;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
@@ -11,12 +10,10 @@ import org.springframework.stereotype.Controller;
 @Controller
 public abstract class AbstractController {
 
-	protected Logger logger = Logger.getLogger(this.getClass().getName());
+	protected Logger logger = Logger.getLogger(this.getClass());
 
 	@Autowired
 	protected HttpServletRequest request;
-//	@Autowired
-	protected HttpServletResponse response;
 	@Autowired
 	protected HttpSession session;
 }
