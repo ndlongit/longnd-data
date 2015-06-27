0ef8239634ebb3d26d70af3acea6f470b130e3d6
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2015-06-27 04:49:22 +0000

    OK

--- Temp/Transfer/sedex-web/src/main/java/com/sedex/appexch/web/controller/AppInfoController.java
+++ Temp/Transfer/sedex-web/src/main/java/com/sedex/appexch/web/controller/AppInfoController.java
@@ -1,5 +1,6 @@
 package com.sedex.appexch.web.controller;
 
+import org.apache.log4j.Level;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,13 +11,11 @@ import com.sedex.appexch.web.controller.base.AbstractController;
 @Controller
 public class AppInfoController extends AbstractController {
 
-	private static final String BASE = "app";
+	private static final String NAME_SPACE = "app";
 
 	@RequestMapping(value = "/apps", method = RequestMethod.GET)
 	public ModelAndView listApps() {
-		System.out.println("request = " + request);
-		System.out.println("response = " + response);
-		System.out.println("session = " + session);
-		return new ModelAndView(BASE + "/listApps");
+		logger.log(Level.WARN, "[listApps]");
+		return new ModelAndView(NAME_SPACE + "/listApps");
 	}
 }
