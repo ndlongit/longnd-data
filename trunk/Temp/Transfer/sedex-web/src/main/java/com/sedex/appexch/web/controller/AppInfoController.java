c8fe1bbdf66c34bf711be96685797a516bf9ea12
ndlongit <ndlongit@0678d5b6-dab6-a150-5438-a0108a10141d>
2015-06-28 12:01:15 +0000

    Change action names and relating items

--- trunk/Temp/Transfer/sedex-web/src/main/java/com/sedex/appexch/web/controller/AppInfoController.java
+++ trunk/Temp/Transfer/sedex-web/src/main/java/com/sedex/appexch/web/controller/AppInfoController.java
@@ -18,19 +18,32 @@ import com.sedex.appexch.service.AppInfoService;
 import com.sedex.appexch.web.controller.base.AbstractController;
 
 @Controller
-@RequestMapping(value = "/" + AppInfoController.NAME_SPACE + "/")
+@RequestMapping(value = AppInfoController.NAME_SPACE)
 public class AppInfoController extends AbstractController {
 
-    protected static final String NAME_SPACE = "app";
+    protected static final String REAL_NAME_SPACE = "/app";
+    protected static final String NAME_SPACE = ADMIN_NAME_SPACE + REAL_NAME_SPACE;
+
     private static final String MODEL_NAME = "AppInfo";
 
     @Resource
     private AppInfoService appInfoService;
 
+    /** Default handler for "/" */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView init(@ModelAttribute(MODEL_NAME) AppInfo app, BindingResult result) {
+        // TODO Modify later
+        ModelAndView mnv = initModelAndView(REAL_NAME_SPACE + "/uploadApp");
+        String action = getFullUrl(NAME_SPACE, CREATE);
+        mnv.addObject("action", action);
+
+        return mnv;
+    }
+
     @RequestMapping(value = PRE_CREATE, method = RequestMethod.GET)
     public ModelAndView createAppPre(@ModelAttribute(MODEL_NAME) AppInfo app, BindingResult result) {
-        ModelAndView mnv = initModelAndView(NAME_SPACE + "/uploadApp");
-        String action = getFullUrl(NAME_SPACE + "/" + CREATE);
+        ModelAndView mnv = initModelAndView(REAL_NAME_SPACE + "/uploadApp");
+        String action = getFullUrl(NAME_SPACE, CREATE);
         mnv.addObject("action", action);
 
         return mnv;
@@ -46,24 +59,24 @@ public class AppInfoController extends AbstractController {
             e.printStackTrace();
         }
 
-        ModelAndView mnv = initModelAndView(NAME_SPACE + "/listApps");
+        ModelAndView mnv = initModelAndView(REAL_NAME_SPACE + "/listApps");
         return mnv;
     }
 
     @RequestMapping(value = PRE_EDIT + "/{id}", method = RequestMethod.GET)
     public ModelAndView editAppPre(@PathVariable("id") String appId) {
         logger.info("appId = " + appId);
-        return new ModelAndView(NAME_SPACE + "/uploadApp");
+        return new ModelAndView(REAL_NAME_SPACE + "/uploadApp");
     }
 
     @RequestMapping(value = EDIT + "/{id}", method = RequestMethod.POST)
     public ModelAndView editApp() {
-        return new ModelAndView(NAME_SPACE + "/uploadApp");
+        return new ModelAndView(REAL_NAME_SPACE + "/uploadApp");
     }
 
     @RequestMapping(value = LIST, method = RequestMethod.GET)
     public ModelAndView listApps(@ModelAttribute(MODEL_NAME) AppInfo app, BindingResult result) {
-        ModelAndView mnv = initModelAndView(NAME_SPACE + "/listApps");
+        ModelAndView mnv = initModelAndView(REAL_NAME_SPACE + "/listApps");
         List<AppInfo> apps = appInfoService.findAll();
         mnv.addObject("dataList", apps);
 
