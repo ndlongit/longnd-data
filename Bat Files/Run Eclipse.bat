b1d6423ff35465e5898d25af5404454c279dfb5c
longnd-softqware <Long.nguyen@softqware.com>
2015-07-05 22:43:14 +0700

    add Android Emulator Tools

--- Bat Files/Run Eclipse.bat
+++ Bat Files/Run Eclipse.bat
@@ -1,10 +1,8 @@
 @echo off
 
-::set ECLIPSE_HOME="D:\Software\Eclipse\Eclipse 4.2"
-::set ECLIPSE_HOME="d:\Software\Eclipse\Eclipse PHP 4.3"
-set ECLIPSE_HOME="d:\Software\Eclipse\Eclipse 4.3 - X64"
+set ECLIPSE_HOME="d:\Software\Eclipse\eclipse-jee-luna-SR2-win32-x86_64"
 
-set WORK_SPACE="d:\Data-Store\Workspace"
+set WORK_SPACE="d:\Workspace\common-workspace"
 ::set WORK_SPACE="d:\Data-Store\PHP-Workspace"
 
 cd /d %ECLIPSE_HOME% 
